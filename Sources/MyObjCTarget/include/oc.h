02d1cc17eec2bafa46127c13ca2be4196fc7ff23
uncleblack <apelovefox@gmail.com>
2025-08-10 02:11:19 +0800

    add comments

--- Sources/MyObjCTarget/include/oc.h
+++ Sources/MyObjCTarget/include/oc.h
@@ -1,3 +1,6 @@
+// https://stackoverflow.com/questions/1742890/cgwindowid-from-axuielement
+// https://github.com/rxhanson/Rectangle/blob/main/Rectangle/Rectangle-Bridging-Header.h
+
 #import <AppKit/AppKit.h>
 
 // extern "C" AXError _AXUIElementGetWindow(AXUIElementRef, CGWindowID* out);
