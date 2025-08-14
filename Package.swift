c76105ea0c1aeeea1130cf8a141f9d667cc20c80
hangj <guijie.han@gmail.com>
2025-08-14 14:41:52 +0800

    update macOS version to v11

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "SwiftElementAccess",
     platforms: [
-        .macOS(.v10_11) // Minimum macOS version required
+        .macOS(.v11) // Minimum macOS version required
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
