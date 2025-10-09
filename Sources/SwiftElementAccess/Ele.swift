171c0c74a4f254304e36b114f64ac9fba5da76e4
uncleblack <apelovefox@gmail.com>
2025-10-10 00:21:53 +0800

    revert the last change

--- Sources/SwiftElementAccess/Ele.swift
+++ Sources/SwiftElementAccess/Ele.swift
@@ -206,27 +206,10 @@ extension AXUIElement {
         return ret
     }
 
-    public static var frontmostApplication: NSRunningApplication?
-
     public static func fromFrontMostApplication() -> AXUIElement? {
-        struct Once {
-            static let once = {
-                AXUIElement.frontmostApplication = NSWorkspace.shared.frontmostApplication
-                // https://stackoverflow.com/questions/32533525/nsworkspaces-frontmostapplication-doesnt-change-after-initial-use
-                NSWorkspace.shared.notificationCenter.addObserver(forName: NSWorkspace.didActivateApplicationNotification, object: nil, queue: nil) { noti in
-                    AXUIElement.frontmostApplication = noti.userInfo?[NSWorkspace.applicationUserInfoKey] as? NSRunningApplication
-                }
-
-                return true
-            }()
-        }
-
-        let _ = Once.once
-
-        if let app = frontmostApplication {
+        if let app = NSWorkspace.shared.frontmostApplication {
             return AXUIElement.fromPid(app.processIdentifier)
         }
-
         return nil
     }
 
